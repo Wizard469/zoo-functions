50e7ead595c1a4b2530868f47a9f0add417f45df
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-20 18:15:16 -0300

    Second requirement

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return data.species.find(({ name }) => animal === name).residents
+    .every(({ age: animalAges }) => age <= animalAges);
 }
 
 module.exports = getAnimalsOlderThan;
