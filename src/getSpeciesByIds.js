ac9091a25d3912843e1e1220871d6ec7b877b081
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-20 17:02:57 -0300

    First requirement

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aquii
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+  if (ids === undefined) return [];
+  return ids.map((element) => data.species.find(({ id }) => id === element));
 }
 
 module.exports = getSpeciesByIds;
