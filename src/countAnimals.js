987fcae0d2ef5ba1937b635775cbe3bf4d527e68
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-21 17:40:39 -0300

    Fifth requirement

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals({ specie, sex } = {}) {
+  const allAnimals = {};
+  if (!specie && !sex) {
+    species.forEach(({ name, residents }) => {
+      allAnimals[name] = residents.length;
+    });
+    return allAnimals;
+  }
+
+  if (!sex) {
+    return species.find(({ name }) => name === specie).residents.length;
+  }
+
+  return species.find(({ name }) => name === specie).residents
+    .filter((bothSex) => bothSex.sex === sex).length;
 }
 
 module.exports = countAnimals;
