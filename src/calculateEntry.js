c214643814a422bc18d3b00c749f54f03c8e5dbd
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-22 15:15:15 -0300

    Eighth requirement

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter(({ age }) => age < 18).length,
+    adult: entrants.filter(({ age }) => age >= 18 && age < 50).length,
+    senior: entrants.filter(({ age }) => age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+
+  const { child: price1, adult: price2, senior: price3 } = prices;
+  const { child, adult, senior } = countEntrants(entrants);
+
+  return child * price1 + adult * price2 + senior * price3;
 }
 
 module.exports = { calculateEntry, countEntrants };
