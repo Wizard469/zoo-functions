b4c41f1e9a9b8939ae1fadb84cebea72774ac07b
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-25 12:52:55 -0300

    Eleventh requirement

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,29 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const employeeInfo = () => employees.map((employee) => (
+  {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: employee.responsibleFor.map((animal) => species.find(({ id }) => id === animal))
+      .map(({ name }) => name),
+    locations: employee.responsibleFor.map((animal) => species
+      .find(({ id }) => id === animal)).map(({ location }) => location),
+  }
+));
+
+function getEmployeesCoverage(information) {
+  if (!information) return employeeInfo();
+
+  const { id, name } = information;
+
+  if (employeeInfo().find((employee) => employee.id === id)) {
+    return employeeInfo().find((employee) => employee.id === id);
+  }
+  if (employeeInfo().find((employee) => employee.fullName.includes(name))) {
+    return employeeInfo().find((employee) => employee.fullName.includes(name));
+  }
+
+  throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
